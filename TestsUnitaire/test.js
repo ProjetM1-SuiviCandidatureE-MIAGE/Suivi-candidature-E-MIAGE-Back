26ade9cad6f15d5ece0944c387d376e376ca3454
DR4KEN <pogoman28@gmail.com>
2019-04-11 10:04:38 +0200

    Debut des tests

---
+++ TestsUnitaire/test.js
@@ -0,0 +1,23 @@
+var assert = require('chai').assert;
+var should = require('chai').should();
+
+
+describe('test du renvoi des candidatures avec la fonction getAllCandidatures',function(){
+  it('doit renvoyer des candidatures',function(){
+     
+  })
+})
+
+
+/*describe('Tests for todo.app', function() {
+  describe('the trimTodoName function', function() {
+    it('should remove leading whitespace', function() {
+      todo.util.trimTodoName('   name').should.equal('name');
+    });
+    it('should remove trailing whitespace', function() {
+      todo.util.trimTodoName('name   ').should.have.length(4);
+    });
+    it('should remove leading and trailing whitespace', function() {
+      assert(todo.util.trimTodoName('  name  '), 'name');
+    });
+  })}*/
