24cf65fab48d1553878102eb937178a207faeb5b
DamienDnd <d.donnadieux@gmail.com>
2019-06-11 10:25:03 +0200

    route envoi mail aux admins

--- server/Candidature/Mail.js
+++ server/Candidature/Mail.js
@@ -4,16 +4,17 @@ let bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  console.log("Hi");
-});
-
 app.post("/send", (req, res) => {
-  mail.sendMail(req.body).then((callback) => {
-    res.send(callback);
-  });
+  	mail.sendMail(req.body).then((callback) => {
+    	res.send(callback);
+  	});
 });
 
+app.post("/sendAdmins", (req, res) => {
+	mail.sendMailAllAdmins(req.body.sujet, req.body.texte).then((callback) => {
+    	res.send(callback);
+  	});
+});
 
 module.exports = app;
 
--- server/Candidature/MailProcess.js
+++ server/Candidature/MailProcess.js
@@ -2,51 +2,52 @@ const nodemailer = require("nodemailer");
 const adminFonction = require("../Admin/adminProcess");
 
 
-
+// Fonction pour envoyer un mail
 async function sendMail (body) {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-        user: "gestioncandidaturem1miaa@gmail.com",
-        pass: "azertyM1MIAA"
-      },
-      tls: { rejectUnauthorized: false }
-    });
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "gestioncandidaturem1miaa@gmail.com",
+      pass: "azertyM1MIAA"
+    },
+    tls: { rejectUnauthorized: false }
+  });
   
-    // setup email data with unicode symbols
-    let mailOptions = {
-      from: "gestioncandidaturem1miaa@gmail.com", // sender address
-      to: body.mail, // list of receivers
-      subject: body.sujet, // Subject line
-      //text: "Hello world?", // plain text body
-      html: body.texte // html body
-    };
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: "gestioncandidaturem1miaa@gmail.com", // sender address
+    to: body.mail, // list of receivers
+    subject: body.sujet, // Subject line
+    //text: "Hello world?", // plain text body
+    html: body.texte // html body
+  };
   
-    await transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log("Erreur lors de l'envoi du mail");
-        return console.log(error);
-      } else {
-        console.log("Message sent: %s", info.messageId);
-        return ({ text: "succès, mail envoyé" });
-      }
-    });
-  }
+  await transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Erreur lors de l'envoi du mail");
+      return console.log(error);
+    } else {
+      console.log("Message sent: %s", info.messageId);
+      return ({ text: "succès, mail envoyé" });
+    }
+  });
+}
 
-  async function sendMailAllAdmins(sujet,texte){
-    const admins = await adminFonction.getAllAdmins();
-    console.log(admins);
-    admins.forEach(element => {
-      sendMail(
-        {
-          mail : element.mail,
-          sujet : sujet,
-          texte : texte
-        })
-    });
-    return ({ text: "succès, mails envoyés aux admins" }); 
-  }
+// Fonction pour envoyer un mail à tous les enseignants/admins
+async function sendMailAllAdmins(sujet,texte){
+  const admins = await adminFonction.getAllAdmins();
+  console.log(admins);
+  admins.forEach(element => {
+    sendMail(
+      {
+        mail : element.mail,
+        sujet : sujet,
+        texte : texte
+      })
+  });
+  return ({ text: "succès, mails envoyés aux admins" }); 
+}
   
-  exports.sendMail = sendMail;
-  exports.sendMailAllAdmins = sendMailAllAdmins;
+exports.sendMail = sendMail;
+exports.sendMailAllAdmins = sendMailAllAdmins;
